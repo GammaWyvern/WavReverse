313a5ce9f91e44c16843895c8b25ee137eff2fed
GammaWyvern <keegenjt@gmail.com>
2023-09-06 13:07:47 -0400

    Starting to refactor code to follow assignment rules

--- wav.h
+++ wav.h
@@ -1,6 +1,16 @@
 #ifndef __H_LIB__
 #define __H_LIB__
 
+struct wav_header {
+	char* header;
+}
+
+struct wav_file {
+	struct wav_header* wavHeader;
+	int fileSize;
+	char* dataPointer;
+}
+
 int reverseWav(const char* wavPath, const char* outputPath);
 int validateWav(char* wavByteData);
 
