db0db9761d3cd4b70cf3d9217051acc71108e21f
GammaWyvern <keegenjt@gmail.com>
2023-09-06 22:27:13 -0400

    Added main file

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "wav.h"
+
+int main(int argc, char* argv[]) {
+	if(argc != 3) {
+		printf("\nIncorrect number of arguments.\n");
+		printf("Correct usage is:\n");
+		printf("\treversewav [src .wav file] [dest .wav file]\n\n");
+		return 1;
+	}
+
+	// Create Wave file data
+	struct wave_file* waveFile = load_wave(argv[1]);
+
+	// Verify Wave file data
+	if(!validate_wave_file(waveFile)) {
+		return 1;
+	}
+
+	// Reverse Wave file data and output
+	reverse_wave_file(waveFile, argv[2]);
+
+	// TODO also need to free header???
+	free(waveFile);
+
+	return 0;
+}
